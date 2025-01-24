7773952354067a02345fa07f880bf98ce3a066c8
harshitha9988 <harshitha.uppala@outlook.com>
2025-01-23 21:18:08 -0500

    AND-OR program

---
+++ and or statements.py
@@ -0,0 +1,18 @@
+a=10
+b=5
+c=0
+d=-5
+e=-10
+f=20
+
+if c>5 and b>5:
+    print("the number is greater than 5")
+
+else:
+    print("the numbers are not greater then 5")
+
+if d==10 or e==10:
+    print("the number is equal to 10")
+
+else:
+    print("the numbers are not equal to 10")
